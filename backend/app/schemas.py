7c5dba6877525f8b42b9e0a7ef007799a6714d6f
kingksjo <kamiye06@gmail.com>
2025-07-28 17:19:00 +0100

    Added schemas for genre and prefrences

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,6 +1,7 @@
 import uuid
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import List, Optional
 
 # --- User Schemas ---
 
@@ -28,3 +29,29 @@ class UserResponse(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+# Schema for updating a user's favorite genres during onboarding.
+class UserUpdateGenres(BaseModel):
+    # Expects a JSON object like: {"genres": ["Action", "Drama", "Sci-Fi"]}
+    genres: List[str]
+
+# Schema for creating a new user interaction (like/dislike).
+class InteractionCreate(BaseModel):
+    # Expects a JSON object like: {"tconst": "tt15398776", "interaction_type": "like"}
+    tconst: str
+    interaction_type: str
+
+# Schema for the final recommendation object we send to the frontend.
+#    This includes the enriched data from TMDb.
+class MovieRecommendation(BaseModel):
+    tconst: str
+    primaryTitle: str
+    startYear: int
+    genres: Optional[str] = None # Make genres optional in case some movies don't have them
+    poster_url: Optional[str] = None # This will come from TMDb
+    overview: Optional[str] = None   # This will come from TMDb
+    
+    # This config allows Pydantic to create this schema from an ORM object
+    class Config:
+        from_attributes = True
+
